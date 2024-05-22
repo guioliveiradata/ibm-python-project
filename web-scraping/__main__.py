8010bb6a23b91ea9e251cab9a0c50dfa81d44333
Guilherme de Oliveira <guilhermedtsc@gmail.com>
2024-05-22 20:37:54 -0300

    Building the dataframe

--- web-scraping/__main__.py
+++ web-scraping/__main__.py
@@ -17,4 +17,22 @@ count = 0
 html_page = requests.get(base_url).text
 data = BeautifulSoup(html_page, 'html.parser')
 
+tables = data.find_all('tbody')
+rows = tables[0].find_all('tr')
 
+for row in rows:
+    if count <= 50:
+        col = row.find_all('td')
+        if len(col) != 0:
+            data_dict = {
+                'Average Rank':col[0].contents[0],
+                'Film':col[1].contents[0],
+                'Year':col[2].contents[0]
+            }
+            df1 = pd.DataFrame(data_dict, index=[0])
+            df = pd.concat([df, df1], ignore_index=True)
+        count += 1
+    else:
+        break
+
+print(df)
